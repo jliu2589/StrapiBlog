c43838a38559318fa47a47caa15f2f8373e31577
Jay Liu <jliu2589@gmail.com>
2023-01-03 20:06:46 -0500

    create a slug with a link to all the restaurants

--- components/Restaurant_List.tsx
+++ components/Restaurant_List.tsx
@@ -1,11 +1,16 @@
 import Card from "./Card.js";
+import Link from "next/link.js";
 
 function Restaurant_List({ props }) {
   return (
     <div className="">
       {props.map((res) => (
         <Card key={res.attributes.id}>
-          <h3>{res.attributes.Name}</h3>
+          <Link href={`/restaurants/${res.attributes.Name}`}>
+            <h3 className="text-xl font-bold hover:cursor-pointer">
+              {res.attributes.Name}
+            </h3>
+          </Link>
           <p>{res.attributes.address}</p>
           <p>{res.attributes.rating}</p>
         </Card>
--- pages/restaurants/[restaurant].tsx
+++ pages/restaurants/[restaurant].tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../../components/Layout";
 
 function Restaurants() {
   //const restaurant = data.filter(res => res.slug === slug)
